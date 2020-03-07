4416f2550c8e58fe4d4881de5b98f634421bd589
admin <lijiahui@lijiahuideMacBook-Air.local>
2020-03-07 19:37:12 +0800

    YooData

--- Data/Yoochoose_Create_Data.py
+++ Data/Yoochoose_Create_Data.py
@@ -2,24 +2,24 @@ import codecs
 import os
 
 print('start create data......')
-train = codecs.open('LASTFM/lastfm_train.artist.txt', encoding='utf-8', mode='w')
-with codecs.open('LASTFM/train.artist.txt', encoding='utf-8') as f:
+train = codecs.open('Yoochoose/Yoochoose_train.artist.txt', encoding='utf-8', mode='w')
+with codecs.open('Yoochoose/train.artist.txt', encoding='utf-8') as f:
     for line in f:
         record = line.strip('\n').strip('\r').split(', ')
         for i in range(len(record) - 1):
             train.write('[' + ', '.join(record[0:i+1]) + ']' + '\t' + record[i+1] + os.linesep)
             train.flush()
 train.close()
-dev = codecs.open('LASTFM/lastfm_valid.artist.txt', encoding='utf-8', mode='w')
-with codecs.open('LASTFM/dev.artist.txt', encoding='utf-8') as f:
+dev = codecs.open('Yoochoose/Yoochoose_valid.artist.txt', encoding='utf-8', mode='w')
+with codecs.open('Yoochoose/dev.artist.txt', encoding='utf-8') as f:
     for line in f:
         record = line.strip('\n').strip('\r').split(', ')
         for i in range(len(record)-1):
             dev.write('[' + ', '.join(record[0:i+1]) + ']' + '\t' + record[i+1] + os.linesep)
 
 dev.close()
-test = codecs.open('LASTFM/lastfm_test.artist.txt', encoding='utf-8', mode='w')
-with codecs.open('LASTFM/test.artist.txt', encoding='utf-8') as f:
+test = codecs.open('Yoochoose/Yoochoose_test.artist.txt', encoding='utf-8', mode='w')
+with codecs.open('Yoochoose/test.artist.txt', encoding='utf-8') as f:
     for line in f:
         record = line.strip('\n').strip('\r').split(', ')
         for i in range(len(record)-1):
