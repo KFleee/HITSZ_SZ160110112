f2bcf1244cc653a1b6cc3245fcf8a1ca66cbee11
admin <lijiahui@lijiahuideMacBook-Air.local>
2020-03-07 19:29:57 +0800

    YooData

--- Data/DataHandle.py
+++ Data/DataHandle.py
@@ -1,33 +1,34 @@
-import numpy as np
+import codecs
+import os
 
 
 sess_clicks = {}
-ctr = 0
+count = 0
+item2id = {}
 
 # read session
-with open('yoochoose/yoochoose-data/yoochoose-clicks.dat', 'r') as f:
+with open('Yoochoose/yoochoose-clicks.dat', 'r') as f:
     for line in f.readlines():
         sess_cur = line.split(',')
         sessId = sess_cur[0]
         itemId = sess_cur[2]
+        if itemId not in item2id:
+            item2id[itemId] = str(count)
+            count += 1
         if sessId not in sess_clicks:
-            sess_clicks[sessId] = [itemId]
+            sess_clicks[sessId] = [item2id[itemId]]
         else:
-            sess_clicks[sessId].append(itemId)
-        ctr += 1
-        if ctr % 100 == 0:
-            print(sess_clicks)
-        if ctr > 500:
-            break
+            sess_clicks[sessId].append(item2id[itemId])
+
 
 # filter out session of length 1
-for key in sess_clicks.keys():
-    if len(sess_clicks[key]) <= 1:
+for key in list(sess_clicks.keys()):
+    if len(sess_clicks[key]) < 2:
         del sess_clicks[key]
 
 # count item appear times
 item_count = {}
-for key in sess_clicks.keys():
+for key in list(sess_clicks.keys()):
     item_list = sess_clicks[key]
     for item in item_list:
         if item in item_count:
@@ -36,11 +37,19 @@ for key in sess_clicks.keys():
             item_count[item] = 1
 
 # filter out items that appear less than 5 times
-for key in sess_clicks.keys():
+sess_file = codecs.open('Yoochoose/all.artist.txt', encoding='utf-8', mode='w')
+for key in list(sess_clicks.keys()):
     items_list = sess_clicks[key]
-    filter_items_list = filter(lambda i: item_count[i] >= 5, item_list)
+    filter_items_list = filter(lambda i: item_count[i] >= 5, items_list)
     items_list = list(filter_items_list)
-    if len(item_list) == 1:
-        del sess_clicks[key]
+    if len(items_list) == 1:
+        # del sess_clicks[key]
+        continue
     else:
-        sess_clicks[key] = item_list
+        # sess_clicks[key] = items_list
+        sess_file.write(', '.join(items_list) + os.linesep)
+        sess_file.flush()
+sess_file.close()
+
+
+
--- Data/Lastfm_Split.py
+++ Data/Lastfm_Split.py
@@ -6,10 +6,10 @@ import random
 seed = 123456
 random.seed(seed)
 print('start data split......')
-train = codecs.open('LASTFM/train.artist.txt', encoding='utf-8', mode='w')
-dev = codecs.open('LASTFM/dev.artist.txt', encoding='utf-8', mode='w')
-test = codecs.open('LASTFM/test.artist.txt', encoding='utf-8', mode='w')
-with codecs.open('LASTFM/all.artist.txt', encoding='utf-8') as f:
+train = codecs.open('Yoochoose/train.artist.txt', encoding='utf-8', mode='w')
+dev = codecs.open('Yoochoose/dev.artist.txt', encoding='utf-8', mode='w')
+test = codecs.open('Yoochoose/test.artist.txt', encoding='utf-8', mode='w')
+with codecs.open('Yoochoose/all.artist.txt', encoding='utf-8') as f:
     for line in f:
         r = random.random()
         if r < 0.1:
