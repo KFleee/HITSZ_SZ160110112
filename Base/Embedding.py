361ce023203b339744e80cbcda75f27c99a2f0af
admin <lijiahui@lijiahuideMacBook-Air.local>
2020-05-03 21:02:41 +0800

    使用node2vec作为嵌入层

--- Base/Embedding.py
+++ Base/Embedding.py
@@ -1,33 +1,38 @@
-import codecs
 import os
 import numpy as np
+import gensim
 
 
 class Embeddings:
     Vector = None
 
     def loadvec(self):
-        vec_path = 'Base/item.embeddings'
+        # vec_path = 'Base/item.embeddings'
+        # if not os.path.exists(vec_path):
+        #     print('vec path not exixts')
+        # Embeddings.Vector = {}
+        # with codecs.open(vec_path, encoding='utf-8') as f:
+        #     for line in f:
+        #         record = line.strip('\n').split(' ')
+        #         if len(record) <= 2:
+        #             continue
+        #         else:
+        #             item_id = record[0]
+        #             vec = record[1:]
+        #             vec = list(map(float, vec))
+        #             Embeddings.Vector[str(item_id)] = vec
+        vec_path = '../Result/EMBEDDING_MODEL'
         if not os.path.exists(vec_path):
-            print('vec path not exixts')
-        Embeddings.Vector = {}
-        with codecs.open(vec_path, encoding='utf-8') as f:
-            for line in f:
-                record = line.strip('\n').split(' ')
-                if len(record) <= 2:
-                    continue
-                else:
-                    item_id = record[0]
-                    vec = record[1:]
-                    vec = list(map(float, vec))
-                    Embeddings.Vector[str(item_id)] = vec
+            print('vec path not exist')
+            exit(-1)
+        Embeddings.Vector = gensim.models.Word2Vec.load_word2vec_format(vec_path)
 
     def item2vec(self, input_list):
         if Embeddings.Vector is None:
             self.loadvec()
         result = []
         for input in input_list:
-            result.append(Embeddings.Vector[str(input)])
+            result.append(Embeddings.Vector.wv[str(input)])
         result = np.array(result).astype(np.float32)
         return result
 
